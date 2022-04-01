846e6713dbc00c117af9e7b68c0218bd1a6698e6
SRIHARSHITHABONTHU <99649702+SRIHARSHITHABONTHU@users.noreply.github.com>
2022-04-01 10:12:56 +0530

    updated with a new method

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -4,8 +4,14 @@ class FirstJavaCode
          public static void main(String...args)
          {
 
-          System.out.println("Hello to Java Programming World");  
+          System.out.println("Hello to Java Programming World"); 
+          FirstJavaCode fjc = new FirstJavaCode();
+          fjc.simpleMethod();
 
           }
+         void simpleMethod()
+{
+System.out.println("From harshi Second pgm");
+}
        
 }
